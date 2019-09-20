c6553cf472ce72c9b8e8bfa39818ab2758999716
Sourav <sanganeria.sourav@gmail.com>
2019-09-21 01:07:19 +0530

    OAuth: Implement OAuth with Passportjs

---
+++ my app/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Google Authentication API. */
+router.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);router.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/", session: false }),
+    function(req, res) {
+        var token = req.user.token;
+        res.redirect("http://localhost:3000?token=" + token);
+    }
+);
+
+module.exports = router;
