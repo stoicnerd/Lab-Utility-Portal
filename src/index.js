810acb1dbfa6a43a655cb8bc1b9dead8c0db7bae
Niral Khambhati <f20170130@hyderabad.bits-hyderabad.ac.in>
2019-09-21 02:39:16 +0530

    Fixed: Incomplete commit

--- src/index.js
+++ src/index.js
@@ -17,4 +17,4 @@ import { BrowserRouter } from "react-router-dom";ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
